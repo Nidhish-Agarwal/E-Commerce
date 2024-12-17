fc9054008db1709b81cec7cda347accc777be03e
Nidhish-Agarwal <nidhishagarwal14@gmail.com>
2024-12-17 13:16:20 +0530

    Completed Milestone - 7

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -2,13 +2,20 @@ const express = require("express");
 const {
   CreateUser,
   verifyUserController,
+  signup,
+  login,
 } = require("../controllers/user.controller.js");
 const upload = require("../middlewares/multer.js");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 
-router.post("/create-user", upload.single("file"), (req,res)=>{CreateUser(req,res)});
+router.post("/create-user", upload.single("file"), (req, res) => {
+  CreateUser(req, res);
+});
 
 router.get("/activation/:token", verifyUserController);
+
+router.post("/signup", signup);
+router.post("/login", login);
 
 module.exports = router;
