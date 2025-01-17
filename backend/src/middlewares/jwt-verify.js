4a025d35b26dcb109c0ab4594798ee91e0f1c9f5
Nidhish-Agarwal <nidhishagarwal14@gmail.com>
2025-01-17 15:40:52 +0530

    Completed Milestpone - 17

--- backend/src/middlewares/jwt-verify.js
+++ backend/src/middlewares/jwt-verify.js
@@ -4,13 +4,14 @@ if (process.env.NODE !== "PRODUCTION") {
     path: "./config/.env",
   });
 }
-const verifyUser = (err, req, res, next) => {
-  if (req.body.token) {
+const verifyUser = (req, res, next) => {
+  const { token } = req.query;
+  if (!token) {
     return res.status(404).send({ message: "Send token over rqeuest" });
   }
   const data = jwt.verify(req.body.token, process.env.SECRET_KEY);
   req.userEmailAddress = data.email;
-  //   req.body.userEmailAddress
+  req.UserId = data.id;
   next();
 };
 module.exports = verifyUser;
