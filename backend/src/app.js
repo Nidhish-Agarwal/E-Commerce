062ec38d5e08f2b93f08d42911658148a241829b
Nidhish-Agarwal <nidhishagarwal14@gmail.com>
2025-02-03 15:12:27 +0530

    Compelted Milestone 26

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 const userRouter = require("./routes/user.route.js");
 const productRouter = require("./routes/product.route.js");
 const cartRouter = require("./routes/cart.route.js");
+const orderRouter = require("./routes/orders.route.js");
 
 app.get("/", (req, res) => {
   res.send("Welcome to backend");
@@ -24,5 +25,6 @@ app.get("/", (req, res) => {
 app.use("/user", userRouter);
 app.use("/product", productRouter);
 app.use("/cart", cartRouter);
+app.use("/orders", orderRouter);
 
 module.exports = app;
--- backend/src/routes/orders.route.js
+++ backend/src/routes/orders.route.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { verifyToken } = require("../middlewares/jwt-verify.js");
-const { CreateOrder } = require("../controllers/order.controller.js");
+const {
+  CreateOrderController,
+  GetUserOrdersController,
+} = require("../controllers/order.controller.js");
+
 // POST - /confirm-order {addrss, items, totalAmount}
-router.post("/confirm-order", verifyToken, CreateOrder);
+
+router.post("/confirm-order", verifyToken, CreateOrderController);
+router.get("/user-order-data", verifyToken, GetUserOrdersController);
+
 module.exports = router;
